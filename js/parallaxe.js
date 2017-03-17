a21abad0fabe1ebf47ced41c92e764db636d0481
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2017-03-17 18:12:21 +0000

    Improving parallaxe effect, adding inverting option

--- js/parallaxe.js
+++ js/parallaxe.js
@@ -2,15 +2,11 @@
  * Created by mehdi on 07/03/17.
  */
 
-var windowWidth = window.innerWidth
-    || document.documentElement.clientWidth
-    || document.body.clientWidth;
+var windowWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 
-var windowHeight = window.innerHeight
-    || document.documentElement.clientHeight
-    || document.body.clientHeight;
+var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
-function parallaxe(element,moveDegree){
+function parallaxe(element,moveDegree,invert){
     var X=parseFloat(element.style.left),Y=parseFloat(element.style.top); //X and Y are initial positions of the element
 
     document.addEventListener('mousemove',function(e){
@@ -18,8 +14,15 @@ function parallaxe(element,moveDegree){
         var posX=e.clientX-windowWidth/2,posY=e.clientY-windowHeight/2; //posX and posY are relative cursor position from window's center
 
         text.innerHTML="Window's width: "+windowWidth+"px, Window's height: "+windowHeight+"px<br/> Cursor position: x="+posX+", y="+posY;
-        element.style.top=Y-posY/(10*moveDegree)+"px";
-        element.style.left=X-posX/(10*moveDegree)+"px";
+        if(invert){
+            element.style.top=Y+posY*(moveDegree)/500+"px";
+            element.style.left=X+posX*(moveDegree)/200+"px";
+        }
+        else{
+            element.style.top=Y-posY*(moveDegree)/500+"px";
+            element.style.left=X-posX*(moveDegree)/200+"px";
+        }
+
 
     },false);
 
