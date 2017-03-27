74e72e40eadf01ef9853b89fb91fe1614bb747e8
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2017-03-27 22:45:32 +0100

    Relative position is supported now

--- js/main.js
+++ js/main.js
@@ -1,15 +1,23 @@
-var container=document.getElementById("container");
-var layers=container.getElementsByClassName("layer");
-for(var i=0;i<layers.length;i++){
+function executeParallaxeOn(container) {
 
-    var invert= layers[i].getAttribute("invert")===null ? false : (layers[i].getAttribute("invert")!="false");
+    var layers = container.getElementsByClassName("layer");
 
-    layers[i].addEventListener('click',parallaxe(layers[i],parseFloat(layers[i].getAttribute("degree")),invert),false);
+    for (var i = 0; i < layers.length; i++) {
 
+        layers[i].style.display = "block";
+        layers[i].style.position = i ? "absolute" : "relative";
+
+        //getComputedStyle doesn't work with mozilla etc
+        if (i) {
+            layers[i].style.top =
+                parseFloat(getComputedStyle(layers[0], null).top) + parseFloat(getComputedStyle(container, null).padding) + "px";
+            layers[i].style.left = parseFloat(getComputedStyle(layers[0], null).left) + parseFloat(getComputedStyle(container, null).padding) + "px";
+            // + parseFloat(getComputedStyle(layers[0], null).marginTop)
+        }
+
+        var invert = layers[i].getAttribute("invert") === null ? false : (layers[i].getAttribute("invert") != "false");
+
+        layers[i].addEventListener('click', parallaxe(layers[i], parseFloat(layers[i].getAttribute("degree")), invert), false);
+
+    }
 }
-
-var text=document.getElementById("txt");
-/*
-* In parallaxe function, the second argument is degree, it should be positive. Lower degree <=> Larger movement
-* The third argument is to invert movement direction
-* */
--- js/parallaxe.js
+++ js/parallaxe.js
@@ -7,21 +7,23 @@ var windowWidth = window.innerWidth || document.documentElement.clientWidth || d
 var windowHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
 
 function parallaxe(element,moveDegree,invert){
+
+
+ //   element.style.display="block";
+
     var X=parseFloat(element.style.left),Y=parseFloat(element.style.top); //X and Y are initial positions of the element
 
     document.addEventListener('mousemove',function(e){
 
         var posX=e.clientX-windowWidth/2,posY=e.clientY-windowHeight/2; //posX and posY are relative cursor position from window's center
-
-        text.innerHTML="Window's width: "+windowWidth+"px, Window's height: "+windowHeight+"px<br/> Cursor position: x="+posX+", y="+posY;
         if(invert){
-            element.style.top=Y+posY*(moveDegree)/500+"px";
-            element.style.left=X+posX*(moveDegree)/200+"px";
+            posX=-posX;
+            posY=-posY;
         }
-        else{
-            element.style.top=Y-posY*(moveDegree)/500+"px";
-            element.style.left=X-posX*(moveDegree)/200+"px";
-        }
+
+        element.style.top=Y+posY*(moveDegree)/500+"px";
+        element.style.left=X+posX*(moveDegree)/200+"px";
+
 
 
     },false);
