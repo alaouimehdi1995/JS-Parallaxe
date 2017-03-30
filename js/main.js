9686be1c222af2d8d0e7d83ec794eaf2b769fe4f
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2017-03-30 03:35:51 +0100

    fixed relative (first) layer movement issue

--- js/main.js
+++ js/main.js
@@ -5,12 +5,9 @@ function parallaxeElement(element,version) {
 
         layers[i].style.position = i ? "absolute" : "relative"; //First Element will be relative, and all others absolute
 
-        if (i) {
+        layers[i].style.top =parseFloat(window.getComputedStyle(layers[0],null).top) + parseFloat(window.getComputedStyle(container,null).paddingTop) + "px";
+        layers[i].style.left = parseFloat(window.getComputedStyle(layers[0],null).left) + parseFloat(window.getComputedStyle(container,null).paddingLeft) + "px";
 
-            layers[i].style.top =parseFloat(window.getComputedStyle(layers[0],null).top) + parseFloat(window.getComputedStyle(container,null).paddingTop) + "px";
-            layers[i].style.left = parseFloat(window.getComputedStyle(layers[0],null).left) + parseFloat(window.getComputedStyle(container,null).paddingLeft) + "px";
-
-        }
 
         var invert = layers[i].getAttribute("invert") === null ? false : (layers[i].getAttribute("invert") != "false");
         if(version==="desktop")
