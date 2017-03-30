0018fe55cc9966266120663ee2bbb85c58103f3e
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2017-03-30 03:09:19 +0100

    Improving moving sensibility in mobileParallaxe()

--- js/mobileParallaxe.js
+++ js/mobileParallaxe.js
@@ -1,50 +1,53 @@
 
 function truncate(number,precision){
-    return parseFloat(parseInt(number*Math.pow(10,precision))/Math.pow(10,precision));
+    return parseFloat(parseInt(number*Math.pow(100,precision))/Math.pow(100,precision));
 }
 
 
-function mobileParallaxe(element,moveDegree,invert){
-    var sensibility=0.4;
-    var initMotion={
+function mobileParallaxe(element,moveDegree,invert,sensibility){
+    if(!sensibility){
+        sensibility=0.9;
+    }
+    var initialAccelerometer={
         x:null,
         y:null,
         isFirst:true
     };
-    window.addEventListener('devicemotion', function (e) {
-        if(initMotion.isFirst){
-            initMotion.x=parseInt(truncate(6*Math.PI*e.accelerationIncludingGravity.x,4));
-            initMotion.y=parseInt(truncate(6*Math.PI*e.accelerationIncludingGravity.y,4));
-            initMotion.isFirst=false;
+    window.addEventListener('devicemotion', function (e) { //We save the initial device's position
+        if(initialAccelerometer.isFirst){
+            initialAccelerometer.x=parseInt(truncate(6*Math.PI*e.accelerationIncludingGravity.x,4));
+            initialAccelerometer.y=parseInt(truncate(6*Math.PI*e.accelerationIncludingGravity.y,4));
+            initialAccelerometer.isFirst=false;
         }
 
     }, false);
 
 
-    var initial={
+    var initial={                                       //We save the initial css position of the element
         x:parseFloat(element.style.left),
         y:parseFloat(element.style.top)
     };
 
     window.addEventListener('devicemotion',function(e){
 
+
         var position={              //position.x and position.y are relative cursor position from window's center
-            x:truncate(6*Math.PI*e.accelerationIncludingGravity.x,4)-initMotion.x,
-            y:truncate(6*Math.PI*e.accelerationIncludingGravity.y,4)-initMotion.y
+            x:parseInt(sensibility*parseInt(truncate(6*Math.PI*e.accelerationIncludingGravity.x-initialAccelerometer.x,3))),
+            y:parseInt(sensibility*parseInt(truncate(6*Math.PI*e.accelerationIncludingGravity.y-initialAccelerometer.y,3)))
         };
 
         var txt=document.getElementById("coord");
-        txt.innerHTML="DeviceMotionEvent:(first(x:"+initMotion.x+",y:"+initMotion.y+"))<br/>"
+        txt.innerHTML="DeviceMotionEvent:(first(x:"+initialAccelerometer.x+",y:"+initialAccelerometer.y+"))<br/>"
         txt.innerHTML+="\tAccelerometre:<br/>x:" +position.x +"<br/>\ty:" +position.y;//+"<br/>\tz:";
 
-
+        position.y=-position.y;
         if(invert){
             position.x=-position.x;
             position.y=-position.y;
         }
 
-        element.style.top=initial.y+position.y*(moveDegree)/40+"px";
-        element.style.left=initial.x+position.x*(moveDegree)/40+"px";
+        element.style.top=initial.y+position.y*(moveDegree)/50+"px";
+        element.style.left=initial.x+position.x*(moveDegree)/50+"px";
 
 
 
