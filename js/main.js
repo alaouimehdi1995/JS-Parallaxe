c4f3575f6b419393b2eb7e2b02e30e49e5e5afd1
Mehdi ALAOUI <alaoui.mehdi.1995@gmail.com>
2017-03-31 04:49:55 +0100

    Fluidity improved: we have one listener instead of layers number

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ SCREEN.detectMode();
 function parallaxeElement(CONTAINER) {
 
     var LAYERS = CONTAINER.getElementsByClassName("layer");
+    var ELEMENTS=[];
     var ELEMENT;
 
     for (var i = 0; i < LAYERS.length; i++) {
@@ -19,6 +20,10 @@ function parallaxeElement(CONTAINER) {
             SCALE_X: 0,
             SCALE_Y: 0,
             SENSITIVITY: null,
+            INITIAL:{
+                x:null,
+                y:null,
+            },
             extract_data:function(){
                 this.LAYER= LAYERS[i];
                 this.OPACITY= this.LAYER.getAttribute("data-opacity") === null ? (window.getComputedStyle(this.LAYER,null).opacity === null ? 1 : window.getComputedStyle(this.LAYER,null).opacity) : parseFloat(this.LAYER.getAttribute("data-opacity"));
@@ -27,19 +32,30 @@ function parallaxeElement(CONTAINER) {
                 this.SCALE_X= this.LAYER.getAttribute("data-scale-x") === null ? 0 : parseFloat(this.LAYER.getAttribute("data-scale-x"));
                 this.SCALE_Y= this.LAYER.getAttribute("data-scale-y") === null ? 0 : parseFloat(this.LAYER.getAttribute("data-scale-y"));
                 this.SENSITIVITY= this.LAYER.getAttribute("data-sensitivity") === null ? null : parseFloat(this.LAYER.getAttribute("data-sensitivity"));
+
+                if(SCREEN.isTouch){
+                    if(!this.SENSITIVITY)        //If the user doesn't specified a sensitivity, it gets the default value of 0.9
+                        this.SENSITIVITY=0.7;
+                    this.SENSITIVITY=10+20/(this.SENSITIVITY);
+                }
+
+
             }
         };
         ELEMENT.extract_data();
+        //to improve:We should calculate top and left (in case the first element was not positionned with the container
         ELEMENT.LAYER.style.position = i ? "absolute" : "relative"; //First Element will be relative, and all others absolute
         ELEMENT.LAYER.style.top =parseFloat(window.getComputedStyle(LAYERS[0],null).top) + parseFloat(window.getComputedStyle(CONTAINER,null).paddingTop) + ELEMENT.SCALE_Y + "px";
         ELEMENT.LAYER.style.left = parseFloat(window.getComputedStyle(LAYERS[0],null).left) + parseFloat(window.getComputedStyle(CONTAINER,null).paddingLeft) + ELEMENT.SCALE_X + "px";
         ELEMENT.LAYER.style.opacity=ELEMENT.OPACITY;
-        if(SCREEN.isTouch)
-            mobileParallaxe(ELEMENT);
-        else
-            parallaxe(ELEMENT);
-
-
+        ELEMENT.INITIAL.x=parseFloat(ELEMENT.LAYER.style.left);
+        ELEMENT.INITIAL.y=parseFloat(ELEMENT.LAYER.style.top);
+        ELEMENTS.push(ELEMENT);
 
     }
+
+    if(SCREEN.isTouch)
+        mobileParallaxe(ELEMENTS);
+    else
+        parallaxe(ELEMENTS);
 }
--- js/parallaxe.js
+++ js/parallaxe.js
@@ -2,12 +2,7 @@
  * Created by Mehdi ALAOUI on 07/03/17. All rights reserved.
  */
 
-function parallaxe(ELEMENT){
-
-    var INITIALPOSITION={
-        x:parseFloat(ELEMENT.LAYER.style.left),
-        y:parseFloat(ELEMENT.LAYER.style.top)
-    };
+function parallaxe(ELEMENTS){
 
     document.addEventListener('mousemove',function(e){
 
@@ -15,18 +10,43 @@ function parallaxe(ELEMENT){
             x:e.clientX-SCREEN.width/2,
             y:e.clientY-SCREEN.height/2
         };
+        for(var i=0;i<ELEMENTS.length;i++) {
+            if (ELEMENTS[i].INVERT) {
+                position.x = -position.x;
+                position.y = -position.y;
+            }
 
-        if(ELEMENT.INVERT){
-            position.x=-position.x;
-            position.y=-position.y;
+            ELEMENTS[i].LAYER.style.top = ELEMENTS[i].INITIAL.y + position.y * (ELEMENTS[i].DEGREE) / 200 + "px";
+            ELEMENTS[i].LAYER.style.left = ELEMENTS[i].INITIAL.x + position.x * (ELEMENTS[i].DEGREE) / 200 + "px";
+
         }
-
-        ELEMENT.LAYER.style.top=INITIALPOSITION.y+position.y*(ELEMENT.DEGREE)/200+"px";
-        ELEMENT.LAYER.style.left=INITIALPOSITION.x+position.x*(ELEMENT.DEGREE)/200+"px";
-
-
 
     },false);
 
+}
+
+function mobileParallaxe(ELEMENTS){
+
+    window.addEventListener('devicemotion',function(e){ // When the user moves the device
+
+        var CURRENT_POSITION=getActualPosition(e);
+        var PIXELS_TO_MOVE={              //PIXELS_TO_MOVE.x and PIXELS_TO_MOVE.y are the difference between the initial position and the actual one
+            x:parseInt(truncateNumber(6*Math.PI*CURRENT_POSITION.x-SCREEN.INITIAL.x,3)),
+            y:parseInt(truncateNumber(6*Math.PI*CURRENT_POSITION.y-SCREEN.INITIAL.y,3))
+        };
+        for(var i=0;i<ELEMENTS.length;i++) {
+
+            //the layers don't move until rotationRate reachs the sensitivity
+            if ((!SCREEN.isLandscape && (Math.abs(e.rotationRate.beta * 100) > ELEMENTS[i].SENSITIVITY)) || (SCREEN.isLandscape && (Math.abs(e.rotationRate.alpha * 100) > ELEMENTS[i].SENSITIVITY))) {
+
+                ELEMENTS[i].LAYER.style.left = ELEMENTS[i].INITIAL.x + (ELEMENTS[i].INVERT ? -PIXELS_TO_MOVE.x : PIXELS_TO_MOVE.x) * (ELEMENTS[i].DEGREE) / 20 + "px";
+            }
+            if ((!SCREEN.isLandscape && (Math.abs(e.rotationRate.alpha * 100) > ELEMENTS[i].SENSITIVITY)) || (SCREEN.isLandscape && (Math.abs(e.rotationRate.beta * 100) > ELEMENTS[i].SENSITIVITY))) {
+
+                ELEMENTS[i].LAYER.style.top = ELEMENTS[i].INITIAL.y + (ELEMENTS[i].INVERT ? PIXELS_TO_MOVE.y : -PIXELS_TO_MOVE.y) * (ELEMENTS[i].DEGREE) / 20 + "px";
+            }
+        }
+
+    },false);
 
 }
